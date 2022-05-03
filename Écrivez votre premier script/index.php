91cb498a12d1733fac8114a64c7f11b17e980303
GreyStromRenard <91661492+GreyStromRenard@users.noreply.github.com>
2022-05-03 10:00:10 +0200

    edit files

---
+++ Écrivez votre premier script/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8"/>
+            <title>Ma page web</title>
+        </head>
+        <body>
+            <h1>Ma page web</h1>
+    
+            <p>
+                Bonjour Mathys !<br />
+                Cette page contient du code HTML avec des balises PHP.<br />
+                <?php ?>
+                Voici quelques petits tests :
+            </p>
+
+            <ul>
+                <li style="color: blue;">Texte en bleu</li>
+                <li style="color: red;">Texte en rouge</li>
+                <li style="color: green;">Texte en vert</li>
+            </ul>
+        </body>
+    </html>
