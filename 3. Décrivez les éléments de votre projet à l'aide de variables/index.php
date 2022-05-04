d038e416359338c58a195c1b8e4066da1671533d
GreyStromRenard <91661492+GreyStromRenard@users.noreply.github.com>
2022-05-04 10:37:30 +0200

    edit and create files

---
+++ 3. Décrivez les éléments de votre projet à l'aide de variables/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$userAge = 17;
+$calc = ($userAge + 5) * $userAge;
+
+$fullName = "Mathys Wagner";
+$email = 'mathys.wagner@mmibordeaux.com';
+
+$price = 27.99;
+
+$isAuthor = true;
+$isAdministrator = false;
+
+$noValue = NULL;
+
+echo 'Bonjour, mon "nom" est ' . $fullName . ' !' ;
+
+echo "Le prix est de ";
+echo $price;
+echo " €. ";
+
+echo 'J\'ai ' . $calc . ' ans.';
