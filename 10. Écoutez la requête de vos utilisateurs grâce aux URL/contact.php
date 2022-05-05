b02bd5bbe7269af0578282cddda6b9e5cd50cbcf
GreyStromRenard <91661492+GreyStromRenard@users.noreply.github.com>
2022-05-05 09:46:14 +0200

    added

---
+++ 10. Écoutez la requête de vos utilisateurs grâce aux URL/contact.php
@@ -0,0 +1,11 @@
+<form action="submit_contact.php" method="GET">
+    <div>
+        <label for="email">Email</label>
+        <input type="text" name="email" placeholder="Votre Email...">
+    </div>
+    <div>
+        <label for="message">Votre message</label>
+        <textarea name="message" placeholder="Exprimez vous..."></textarea>
+    </div>
+    <button type="submit">Envoyer</button>
+</form>
---
+++ 10. Écoutez la requête de vos utilisateurs grâce aux URL/index.php
@@ -0,0 +1,8 @@
+<form action="contact.php" method="GET">
+    <input type="text" name="nom" placeholder="Votre nom..."><br><br>
+    <input type="text" name="prenom" placeholder="Votre prénom...">
+</form>
+
+<h3><a href="contact.php">Formulaire de contact</a></h3>
+
+<!-- <a href="bonjour.php?nom=Dupont&amp;prenom=Jean">Dis-moi bonjour !</a> -->
